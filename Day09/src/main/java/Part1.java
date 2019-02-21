c3618b1131c5a8417e06d10fd36a33feb2ee1398
UvulaBob <sir.paulimedes@gmail.com>
2019-02-21 16:13:23 -0500

    Add files via upload

---
+++ Day09/src/main/java/Part1.java
@@ -0,0 +1,51 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Part1 {
+
+    public static void main(String[] args) throws IOException {
+        String input = Files.readAllLines(Paths.get(new File("").getAbsolutePath() + "\\src\\main\\resources\\input.txt")).get(0);
+
+        int totalScore = 0;
+        int currentGroupScore = 0;
+        boolean garbageMode = false;
+        boolean cancelMode = false;
+        for (String character : input.split("")) {
+            if (cancelMode) {
+                cancelMode = false;
+                continue;
+            }
+
+            switch (character) {
+                case "!":
+                    cancelMode = true;
+                    break;
+                case "{":
+                    if (!garbageMode) {
+                        currentGroupScore++;
+                    }
+                    break;
+                case "}":
+                    if (!garbageMode) {
+                        totalScore += currentGroupScore;
+                        currentGroupScore--;
+                    }
+                    break;
+                case "<":
+                    if (!garbageMode) {
+                        garbageMode = true;
+                    }
+                    break;
+                case ">":
+                    garbageMode = false;
+                    break;
+            }
+        }
+
+        System.out.println(totalScore);
+        System.out.println("Done!");
+
+    }
+}
---
+++ Day09/src/main/java/Part2.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Part2 {
+
+    public static void main(String[] args) throws IOException {
+        String input = Files.readAllLines(Paths.get(new File("").getAbsolutePath() + "\\src\\main\\resources\\input.txt")).get(0);
+
+        int garbageCharacters = 0;
+        boolean garbageMode = false;
+        boolean cancelMode = false;
+        for (String character : input.split("")) {
+            if (cancelMode) {
+                cancelMode = false;
+                continue;
+            }
+
+            switch (character) {
+                case "!":
+                    cancelMode = true;
+                    break;
+                case "{":
+                    if (garbageMode) {
+                        garbageCharacters++;
+                    }
+                    break;
+                case "}":
+                    if (garbageMode) {
+                        garbageCharacters++;
+                    }
+                    break;
+                case "<":
+                    if (garbageMode) {
+                        garbageCharacters++;
+                    }
+                    garbageMode = true;
+                    break;
+                case ">":
+                    garbageMode = false;
+                    break;
+                default:
+                    if (garbageMode) {
+                        garbageCharacters++;
+                    }
+            }
+        }
+
+        System.out.println(garbageCharacters);
+        System.out.println("Done!");
+
+    }
+}
